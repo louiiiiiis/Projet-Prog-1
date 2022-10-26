29ec996df8eac7efa114ba5b4ae57285a1817ce4
Louis <louis.lachaize@gmail.com>
2022-10-26 18:10:43 +0200

    Maj 26 oct

--- asyntax.ml
+++ asyntax.ml
@@ -1,5 +1,3 @@
-open Lexer
-
 (* Définition du type qui nous permet de représenter un arbre syntaxique *)
 type ast =
   | Aint of string
@@ -15,7 +13,7 @@ type ast =
   | Asubf of ast * ast
   | Amulf of ast * ast
   | Aiof of ast
-  | Afoi of ast
+  | Afoi of ast
 
 
 
@@ -24,8 +22,8 @@ let extr_brace l =
   let e = ref [] in
   let rec aux i = function
     | []          -> failwith "Erreur d'analyse syntaxique.\nMauvais parenthésage." 
-    | Lbrace :: t -> e := Lbrace :: !e; aux (i + 1) t
-    | Rbrace :: t -> if i = 1 then t else (e := Rbrace :: !e; aux (i - 1) t)
+    | Lexer.Lbrace :: t -> e := Lexer.Lbrace :: !e; aux (i + 1) t
+    | Lexer.Rbrace :: t -> if i = 1 then t else (e := Lexer.Rbrace :: !e; aux (i - 1) t)
     | h :: t      -> e := h :: !e; aux i t
   in let t = aux 1 l in
   (List.rev !e, t)
@@ -34,28 +32,28 @@ let extr_brace l =
 (* L'indicateur de bloc vaut 0 pour un entier, 1 pour un flottant, 2 pour un bloc du type (exp), 3 pour int(exp), 4 pour float(exp), 5 pour +(exp) et 6 pour -(exp) *)
 let extr = function
     | []           -> failwith "Erreur d'analyse syntaxique.\nPas d'expression à évaluer"
-    | Int s :: t   -> (0, [Int s], t)
-    | Float s :: t -> (1, [Float s] , t)
-    | Lbrace :: t  -> let (exp, tt) = extr_brace t in (2, exp, tt)
-    | Rbrace :: t  -> failwith "Erreur d'analyse syntaxique.\nMauvais parenthésage."
-    | Iof :: t     -> match t with
-			| Lbrace :: tt -> let (exp, ttt) = extr_brace tt in (3, exp, ttt)
-			| _ -> failwith "Erreur d'analyse syntaxique.\nMauvaise utilisation de int, qui doit être suivi par une expression entre parenthèses."
-    | Foi :: t     -> match t with
-			| Lbrace :: tt -> let (exp, ttt) = extr_brace tt in (4, exp, ttt)
-			| _            -> failwith "Erreur d'analyse syntaxique.\nMauvaise utilisation de float, qui doit être suivi par une expression entre parenthèses."
-    | Sum :: t     -> match t with
-			| Lbrace :: tt -> let (exp, ttt) = extr_brace tt in (5, exp, ttt)
-			| _            -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par + si elle n'est pas suivie d'une expression entre parenthèses."
-    | Sub :: t     -> match t with
-			| Lbrace :: tt -> let (exp, ttt) = extr_brace tt in (6, exp, ttt)
-			| _            -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par - si elle n'est pas suivie d'une expression entre parenthèses."
-    | Mul :: t     -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par *"
-    | Div :: t     -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par /"
-    | Mod :: t     -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par %"
-    | Sumf :: t    -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par +."
-    | Subf :: t    -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par -."
-    | Mulf :: t    -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par *."
+    | Lexer.Int s :: t   -> (0, [Lexer.Int s], t)
+    | Lexer.Float s :: t -> (1, [Lexer.Float s] , t)
+    | Lexer.Lbrace :: t  -> let (exp, tt) = extr_brace t in (2, exp, tt)
+    | Lexer.Rbrace :: t  -> failwith "Erreur d'analyse syntaxique.\nMauvais parenthésage."
+    | Lexer.Iof :: t     -> (match t with
+			| Lexer.Lbrace :: tt -> let (exp, ttt) = extr_brace tt in (3, exp, ttt)
+			| _                  -> failwith "Erreur d'analyse syntaxique.\nMauvaise utilisation de int, qui doit être suivi par une expression entre parenthèses.")
+    | Lexer.Foi :: t     -> (match t with
+			| Lexer.Lbrace :: tt -> let (exp, ttt) = extr_brace tt in (4, exp, ttt)
+			| _                  -> failwith "Erreur d'analyse syntaxique.\nMauvaise utilisation de float, qui doit être suivi par une expression entre parenthèses.")
+    | Lexer.Sum :: t     -> (match t with
+			| Lexer.Lbrace :: tt -> let (exp, ttt) = extr_brace tt in (5, exp, ttt)
+			| _                  -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par + si elle n'est pas suivie d'une expression entre parenthèses.")
+    | Lexer.Sub :: t     -> (match t with
+			| Lexer.Lbrace :: tt -> let (exp, ttt) = extr_brace tt in (6, exp, ttt)
+			| _                  -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par - si elle n'est pas suivie d'une expression entre parenthèses.")
+    | Lexer.Mul :: t     -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par *"
+    | Lexer.Div :: t     -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par /"
+    | Lexer.Mod :: t     -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par %"
+    | Lexer.Sumf :: t    -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par +."
+    | Lexer.Subf :: t    -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par -."
+    | Lexer.Mulf :: t    -> failwith "Erreur d'analyse syntaxique.\nUne expression ne peut pas commencer par *."
 
 
 
@@ -64,44 +62,46 @@ let extr = function
 let rec tree l =
   let (i, e, t) = extr l in
   let aux i e = match i with
-    | 0 -> match e with
-             | [Int s] -> Aint s
-             | _       -> failwith "Ce n'est pas censé arriver"
-    | 1 -> match e with
-             | [Float s] -> Afloat s
-             | _       -> failwith "Ce n'est pas censé arriver"
+    | 0 -> (match e with
+            | [Lexer.Int s] -> Aint s
+            | _       -> failwith "Ce n'est pas censé arriver")
+    | 1 -> (match e with
+            | [Lexer.Float s] -> Afloat s
+            | _       -> failwith "Ce n'est pas censé arriver")
     | 2 -> tree e
     | 3 -> Aiof (tree e)
     | 4 -> Afoi (tree e)
     | 5 -> Apos (tree e)
     | 6 -> Aneg (tree e)
+    | _ -> failwith "Ce n'est pas censé arriver"
   in eval (aux i e) t
 
   and eval ltree l =
     let aux i e = match i with
-      | 0 -> match e with
-               | [Int s] -> Aint s
-               | _       -> failwith "Ce n'est pas censé arriver"
-      | 1 -> match e with
-               | [Float s] -> Afloat s
-               | _       -> failwith "Ce n'est pas censé arriver"
+      | 0 -> (match e with
+              | [Lexer.Int s] -> Aint s
+              | _       -> failwith "Ce n'est pas censé arriver")
+      | 1 -> (match e with
+              | [Lexer.Float s] -> Afloat s
+              | _       -> failwith "Ce n'est pas censé arriver")
       | 2 -> tree e
       | 3 -> Aiof (tree e)
       | 4 -> Afoi (tree e)
       | 5 -> Apos (tree e)
       | 6 -> Aneg (tree e)
+      | _ -> failwith "Ce n'est pas censé arriver"
     in
     match l with
       | []        -> ltree
-      | Sum :: t  -> Asum (ltree, eval t)
-      | Sub :: t  -> Asub (ltree, eval t)
-      | Sumf :: t -> Asumf (ltree, eval t)
-      | Subf :: t -> Asubf (ltree, eval t)
-      | Mul :: t  -> let (i, e, tt) = extr t in eval Amul(ltree, aux i e) tt
-      | Div :: t  -> let (i, e, tt) = extr t in eval Adiv(ltree, aux i e) tt
-      | Mod :: t  -> let (i, e, tt) = extr t in eval Amod(ltree, aux i e) tt
-      | Mulf :: t -> let (i, e, tt) = extr t in eval Amulf(ltree, aux i e) tt
-      | _         -> failwith "Erreur d'analyse syntaxique.\nDeux expressions ne peuvent pas se suivre sans opérateur entre les deux *)
+      | Lexer.Sum :: t  -> Asum (ltree, tree t)
+      | Lexer.Sub :: t  -> Asub (ltree, tree t)
+      | Lexer.Sumf :: t -> Asumf (ltree, tree t)
+      | Lexer.Subf :: t -> Asubf (ltree, tree t)
+      | Lexer.Mul :: t  -> let (i, e, tt) = extr t in eval (Amul (ltree, aux i e)) tt
+      | Lexer.Div :: t  -> let (i, e, tt) = extr t in eval (Adiv (ltree, aux i e)) tt
+      | Lexer.Mod :: t  -> let (i, e, tt) = extr t in eval (Amod (ltree, aux i e)) tt
+      | Lexer.Mulf :: t -> let (i, e, tt) = extr t in eval (Amulf (ltree, aux i e)) tt
+      | _         -> failwith "Erreur d'analyse syntaxique.\nDeux expressions ne peuvent pas se suivre sans opérateur entre les deux"
 
 
 
--- test.ml
+++ test.ml
@@ -4,8 +4,8 @@ open Asyntax
 
 (* Fontion qui transorme une lexeme en string *)
 let str_of_lexeme = function
-  | Int s   -> s   
-  | Float s -> s
+  | Int s   -> "Int(" ^ s ^ ")"   
+  | Float s -> "Float(" ^ s ^ ")"
   | Lbrace  -> "Lbrace"
   | Rbrace  -> "Rbrace"
   | Iof     -> "Iof"
@@ -30,12 +30,30 @@ let affiche_lexlist l =
   print_string !s
 
 
+(* Fonction qui affiche un ast *)
+let rec affiche_ast = function
+  | Aint s         -> print_string s
+  | Afloat s       -> print_string s
+  | Apos a         -> print_string "+("; affiche_ast a; print_string ")"
+  | Aneg a         -> print_string "-("; affiche_ast a; print_string ")"
+  | Asum (a1, a2)  -> print_string "("; affiche_ast a1; print_string ")"; print_string "+"; print_string "("; affiche_ast a2; print_string ")"
+  | Asub (a1, a2)  -> print_string "("; affiche_ast a1; print_string ")"; print_string "-"; print_string "("; affiche_ast a2; print_string ")"
+  | Amul (a1, a2)  -> print_string "("; affiche_ast a1; print_string ")"; print_string "*"; print_string "("; affiche_ast a2; print_string ")"
+  | Adiv (a1, a2)  -> print_string "("; affiche_ast a1; print_string ")"; print_string "/"; print_string "("; affiche_ast a2; print_string ")"
+  | Amod (a1, a2)  -> print_string "("; affiche_ast a1; print_string ")"; print_string "%"; print_string "("; affiche_ast a2; print_string ")"
+  | Asumf (a1, a2) -> print_string "("; affiche_ast a1; print_string ")"; print_string "+."; print_string "("; affiche_ast a2; print_string ")"
+  | Asubf (a1, a2) -> print_string "("; affiche_ast a1; print_string ")"; print_string "-."; print_string "("; affiche_ast a2; print_string ")"
+  | Amulf (a1, a2) -> print_string "("; affiche_ast a1; print_string ")"; print_string "*."; print_string "("; affiche_ast a2; print_string ")"
+  | Aiof a         -> print_string "Int("; affiche_ast a; print_string ")"
+  | Afoi a         -> print_string "Float("; affiche_ast a; print_string ")"
+
+
 (* Les valeurs de test *)
 let t1 = "5"
 let t2 = "5+2"
-let t3 = "5 /3% 4"
-let t4 = "524 +. 22.01"
-let t5 = "5. * - int(5.)"
+let t3 = "5 + 3% 4-3 *58"
+let t4 = "524. +. 22.01 *. float(-(2))"
+let t5 = "5 * -( int(5.))"
 
 (* Transformation des tests en listes de lexemes *)
 let lt1 = lexeme_list_of_str t1
@@ -49,15 +67,15 @@ let lt5 = lexeme_list_of_str t5
 let at1 = tree lt1
 let at2 = tree lt2
 let at3 = tree lt3
-let at4 = tree lt5
+let at4 = tree lt4
 let at5 = tree lt5
 
 
 (* Affichage *)
-let () = (print_string (t1 ^ " -> "); (affiche_lexlist lt1); print_string " -> \n";
-          print_string (t2 ^ " -> "); (affiche_lexlist lt2); print_string " -> \n";
-          print_string (t3 ^ " -> "); (affiche_lexlist lt3); print_string " -> \n";
-          print_string (t4 ^ " -> "); (affiche_lexlist lt4); print_string " -> \n";
-          print_string (t5 ^ " -> "); (affiche_lexlist lt5); print_string " -> \n")
+let () = (print_string (t1 ^ " -> "); (affiche_lexlist lt1); print_string " -> "; (affiche_ast at1); print_string "\n";
+          print_string (t2 ^ " -> "); (affiche_lexlist lt2); print_string " -> "; (affiche_ast at2); print_string "\n";
+          print_string (t3 ^ " -> "); (affiche_lexlist lt3); print_string " -> "; (affiche_ast at3); print_string "\n";
+          print_string (t4 ^ " -> "); (affiche_lexlist lt4); print_string " -> "; (affiche_ast at4); print_string "\n";
+          print_string (t5 ^ " -> "); (affiche_lexlist lt5); print_string " -> "; (affiche_ast at5); print_string "\n")
 
 

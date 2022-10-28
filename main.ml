95aa123a1d78cf6f4d9996c27c47c3ea53e5df85
Louis <louis.lachaize@gmail.com>
2022-10-28 13:52:31 +0200

    maj 28oct

--- main.ml
+++ main.ml
@@ -6,8 +6,9 @@ open Ast2asm
 
 
 (* On récupère le nom du fichier donnée en entrée *)
-let file = read_line ()
-let () = if not (String.equal (String.sub file ((String.length file) - 4) 4) ".exp") then failwith "\nFichier en entrée invalide.\nUne extention \".exp\" est attendue."
+let file = Sys.argv.(1)
+let () = if ((String.length file) < 4) || not (String.equal (String.sub file ((String.length file) - 4) 4) ".exp")
+           then failwith "\nFichier en entrée invalide.\nUne extention \".exp\" est attendue."
 let file_name = String.sub file 0 ((String.length file) - 4)
 
 (* On récupère ce qui est écrit dans le fichier *)
@@ -24,4 +25,4 @@ let b = ast_ok ast
 (* Finalement on génère le code assembleur *)
 let () = s_of_ast ast file_name
 let () = print_string "Compilation effectuée avec succès.\n"
-let () = print_string ("Le fichier " ^ file_name ^ ".s peut être compilé avec la commande \"gc -no-pie " ^ file_name ^ ".s -o " ^ file_name ^ "\".\n")
+let () = print_string ("Le fichier " ^ file_name ^ ".s peut être compilé avec la commande \"gcc -no-pie " ^ file_name ^ ".s -o " ^ file_name ^ "\".\n")
